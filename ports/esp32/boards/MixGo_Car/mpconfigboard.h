fcb929e7d233135eff143fab8b9e9de2e9e21760
大汉子民 <48112196+dahanzimin@users.noreply.github.com>
2022-09-02 14:00:10 +0800

    add mixgo car

---
+++ ports/esp32/boards/MixGo_Car/mpconfigboard.h
@@ -0,0 +1,11 @@
+// This configuration is for a generic ESP32C3 board with 4MiB (or more) of flash.
+
+#define MICROPY_HW_BOARD_NAME               "MixGo Car"
+#define MICROPY_HW_MCU_NAME                 "ESP32C3"
+
+#define MICROPY_HW_ENABLE_SDCARD            (0)
+#define MICROPY_PY_MACHINE_DAC              (0)
+#define MICROPY_PY_MACHINE_I2S              (0)
+
+#define MICROPY_HW_I2C0_SCL                 (7)
+#define MICROPY_HW_I2C0_SDA                 (6)
